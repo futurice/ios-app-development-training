77e01d8ada56df8eb5bbdfa2eecc1c795684e564
Martin Richter <martin.richter@futurice.com>
2015-09-18 11:46:27 +0300

    Create some fake articles and show their title and description in table view

--- Reader/ArticleListViewController.swift
+++ Reader/ArticleListViewController.swift
@@ -10,19 +10,35 @@ import UIKit
 
 class ArticleListViewController: UITableViewController {
 
+    private let articles: [Article]
+
+    // MARK: Lifecycle
+
+    required init?(coder aDecoder: NSCoder) {
+        let fakeArticle1 = Article(title: "First article", description: "This is just a fake article.")
+        let fakeArticle2 = Article(title: "Second article", description: "Here is another one.")
+        self.articles = [fakeArticle1, fakeArticle2]
+
+        super.init(coder: aDecoder)
+    }
+
     // MARK: - UITableViewDataSource
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // This is how many rows we want in the table
-        return 5
+        return articles.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         // Dequeue a cell (this will auto-create one if none found)
-        let cell = tableView.dequeueReusableCellWithIdentifier("basicCell")!
+        let cell = tableView.dequeueReusableCellWithIdentifier("subtitledCell")!
+
+        // Get the relevant article
+        let article = articles[indexPath.row]
 
         // Configure the cell content
-        cell.textLabel?.text = "Row \(indexPath.row)"
+        cell.textLabel?.text = article.title
+        cell.detailTextLabel?.text = article.description
 
         return cell
     }
