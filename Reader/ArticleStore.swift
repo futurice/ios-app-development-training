a5069989622fda3363f9319b74fff1138632dab6
Martin Richter <martin.richter@futurice.com>
2015-09-18 12:03:19 +0300

    Create Article store with hardcoded articles and success callback

---
+++ Reader/ArticleStore.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleStore.swift
+//  Reader
+//
+//  Created by Martin Richter on 18/09/15.
+//  Copyright © 2015 Futurice. All rights reserved.
+//
+
+import Foundation
+
+class ArticleStore {
+
+    typealias SuccessHandler = [Article] -> Void // a function taking an Article array and returning nothing
+
+    func fetchArticles(success success: SuccessHandler) {
+        // Create some fake articles
+        let fakeArticle1 = Article(title: "First article", description: "This is just a fake article.")
+        let fakeArticle2 = Article(title: "Second article", description: "Here is another one.")
+        let fakeArticles = [fakeArticle1, fakeArticle2]
+
+        // Call the success handler immediately
+        success(fakeArticles)
+    }
+}
