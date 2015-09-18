0ccc38f562b5c5bf4639178c8fa5114a921ea054
Martin Richter <martin.richter@futurice.com>
2015-09-18 11:01:40 +0300

    Create simple Article model

---
+++ Reader/Article.swift
@@ -0,0 +1,14 @@
+//
+//  Article.swift
+//  Reader
+//
+//  Created by Martin Richter on 18/09/15.
+//  Copyright © 2015 Futurice. All rights reserved.
+//
+
+import Foundation
+
+struct Article {
+    let title: String
+    let description: String
+}
