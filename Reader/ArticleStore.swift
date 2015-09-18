4ee076734d300535440a5ad16dbd8a808e91b541
Martin Richter <martin.richter@futurice.com>
2015-09-18 19:30:07 +0300

    Fetch raw data from BBC feed and return empty array for now

--- Reader/ArticleStore.swift
+++ Reader/ArticleStore.swift
@@ -13,20 +13,18 @@ class ArticleStore {
     typealias SuccessHandler = [Article] -> Void // a function taking an Article array and returning nothing
 
     func fetchArticles(success success: SuccessHandler) {
-        // Create some fake articles
-        let fakeArticle1 = Article(
-            title: "First article",
-            description: "This is just a fake article.",
-            url: NSURL(string: "http://www.apple.com")!
-        )
-        let fakeArticle2 = Article(
-            title: "Second article",
-            description: "Here is another one.",
-            url: NSURL(string: "http://www.google.com")!
-        )
-        let fakeArticles = [fakeArticle1, fakeArticle2]
+        let articlesURL = NSURL(string: "http://feeds.bbci.co.uk/news/rss.xml")!
 
-        // Call the success handler immediately
-        success(fakeArticles)
+        let task = NSURLSession.sharedSession().dataTaskWithURL(articlesURL) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let _ = data {
+                print("Got some data :)")
+            }
+
+            success([])
+        }
+
+        task.resume()
     }
 }
