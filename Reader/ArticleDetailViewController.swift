77db9eeb8a0266a21b54322ef288ae4579c07cba
Martin Richter <martin.richter@futurice.com>
2015-09-18 13:41:26 +0300

    Create custom class for article detail view controller and add optional article property

---
+++ Reader/ArticleDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ArticleDetailViewController.swift
+//  Reader
+//
+//  Created by Martin Richter on 18/09/15.
+//  Copyright © 2015 Futurice. All rights reserved.
+//
+
+import UIKit
+
+class ArticleDetailViewController: UIViewController {
+
+    var article: Article?
+}
