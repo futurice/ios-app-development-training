f62d59f89ddd24c72fc3807ab737ac2c8b1c6253
Martin Richter <martin.richter@futurice.com>
2015-09-18 10:36:03 +0300

    Implement table data source to show some cells with row indices as titles

--- Reader/ArticleListViewController.swift
+++ Reader/ArticleListViewController.swift
@@ -8,4 +8,22 @@
 
 import UIKit
 
-class ArticleListViewController: UITableViewController {}
+class ArticleListViewController: UITableViewController {
+
+    // MARK: - UITableViewDataSource
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // This is how many rows we want in the table
+        return 5
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Dequeue a cell (this will auto-create one if none found)
+        let cell = tableView.dequeueReusableCellWithIdentifier("basicCell")!
+
+        // Configure the cell content
+        cell.textLabel?.text = "Row \(indexPath.row)"
+
+        return cell
+    }
+}
