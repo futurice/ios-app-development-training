8d615563cc8dccfbb294b611622711b6d6ae8b90
Martin Richter <martin.richter@futurice.com>
2015-09-18 10:30:55 +0300

    Create custom class for table view controller

---
+++ Reader/ArticleListViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ArticleListViewController.swift
+//  Reader
+//
+//  Created by Martin Richter on 18/09/15.
+//  Copyright © 2015 Futurice. All rights reserved.
+//
+
+import UIKit
+
+class ArticleListViewController: UITableViewController {}
